bf12d2d5ec4345155c46ba02f5d298b1772b2e0f
samwaseee <samwaseee@gmail.com>
2024-03-26 20:18:47 +0600

    about section added

--- src/Components/BookDetails.jsx
+++ src/Components/BookDetails.jsx
@@ -1,4 +1,6 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { ToastContainer,toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const BookDetails = () => {
 
@@ -7,40 +9,51 @@ const BookDetails = () => {
     const book = books.find(book => book.bookId == bookId.id);
     console.log(bookId.id, book);
 
+    const success = () => {
+        toast.success("Added successfully");
+    }
+    const error = () => {
+        toast.error("Already added");
+    }
+
     const { bookName, author, image, review, totalPages, rating, category, tags, publisher, yearOfPublishing } = book;
 
     return (
-        <div className="flex sans gap-10 mt-14">
-            <img src={image} className="w-[100vw] max-h-fit bg-[#13131317] p-16 rounded-xl" />
-            <div>
-                <h1 className="fair text-5xl font-bold">{bookName}</h1>
-                <p className="font-semibold text-xl py-5 border-b-2 border-dashed">By : {author}</p>
-                <p className="text-xl py-5 border-b-2 border-dashed">{category}</p>
-                <p className="mt-10"><span className="font-bold">Review : </span>{review}</p>
-                <p className="my-10 border-b-2 border-dashed pb-10"><span className="font-bold mr-7">Tag</span>
-                    <div className="badge badge-outline text-[#22be0a] font-bold mr-3">#{tags[0]}</div>
-                    <div className="badge badge-outline text-[#22be0a] font-bold">#{tags[1]}</div>
-                </p>
-                <div className="flex gap-16">
-                    <div>
-                        <p>Number of Pages: </p>
-                        <p>Publisher: </p>
-                        <p>Year of Publishing: </p>
-                        <p>Rating: </p>
+        <>
+            <div className="flex sans gap-10 mt-14">
+                <img src={image} className="w-[100vw] max-h-fit bg-[#13131317] p-16 rounded-xl" />
+                <div>
+                    <h1 className="fair text-5xl font-bold">{bookName}</h1>
+                    <p className="font-semibold text-xl py-5 border-b-2 border-dashed">By : {author}</p>
+                    <p className="text-xl py-5 border-b-2 border-dashed">{category}</p>
+                    <p className="mt-10"><span className="font-bold">Review : </span>{review}</p>
+                    <p className="my-10 border-b-2 border-dashed pb-10"><span className="font-bold mr-7">Tag</span>
+                        <div className="badge badge-outline text-[#22be0a] font-bold mr-3">#{tags[0]}</div>
+                        <div className="badge badge-outline text-[#22be0a] font-bold">#{tags[1]}</div>
+                    </p>
+                    <div className="flex gap-16">
+                        <div>
+                            <p>Number of Pages: </p>
+                            <p>Publisher: </p>
+                            <p>Year of Publishing: </p>
+                            <p>Rating: </p>
+                        </div>
+                        <div className="font-bold">
+                            <p>{totalPages}</p>
+                            <p>{publisher}</p>
+                            <p>{yearOfPublishing}</p>
+                            <p>{rating}</p>
+                        </div>
                     </div>
-                    <div className="font-bold">
-                        <p>{totalPages}</p>
-                        <p>{publisher}</p>
-                        <p>{yearOfPublishing}</p>
-                        <p>{rating}</p>
+                    <div className="flex gap-5 mt-8">
+                        <button onClick={success} className="btn btn-outline"> Read </button>
+                        <button onClick={error} className="btn btn-info text-white bg-[#50B1C9]">Wishlist</button>
                     </div>
                 </div>
-                <div className="flex gap-5 mt-8">
-                    <button className="btn btn-outline"> Read </button>
-                    <button className="btn btn-info text-white bg-[#50B1C9]">Wishlist</button>
-                </div>
+
             </div>
-        </div>
+            <ToastContainer />
+        </>
     );
 };
 
